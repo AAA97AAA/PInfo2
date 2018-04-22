15d69b380885e9cd13e8e258dfac15f67515de21
Petr Binko <Petrik.Binko@gmail.com>
2018-04-22 14:20:00 +0200

    Fix missing import

--- academi-co/src/main/java/dom/content/ConcreteQuestionThread.java
+++ academi-co/src/main/java/dom/content/ConcreteQuestionThread.java
@@ -12,10 +12,10 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.MapKey;
 import javax.persistence.MapKeyColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
