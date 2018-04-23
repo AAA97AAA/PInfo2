46807e010bddde2ad0854066ec2cbff7532d896e
kaikoveritch <kaiko.goren@gmail.com>
2018-04-23 19:05:51 +0200

    Corrected the path for the 'test' folder.

---
+++ academi-co/src/test/java/dom/content/ConcretePostTest.java
@@ -0,0 +1,14 @@
+package dom.content;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConcretePostTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
