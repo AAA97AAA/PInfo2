213cc28e2c0c085c6eaa76ec06d89384ffb7b83b
Nicolas Cotte <Nicolas.Cotte.1@etu.unige.ch>
2018-04-27 13:23:20 +0200

    Add DB

--- Jenkins/scripts/database/restartDatabase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CONTAINER_NAME='concealed_cader'
-IMAGE_NAME='academi-co-mysql'
-
-docker rm -vf $CONTAINER_NAME
-
-docker run -p 13306:3306 --name=$CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=admin -d $IMAGE_NAME
