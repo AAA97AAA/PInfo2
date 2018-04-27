20acd53f570c24b7f8db6ebc1e854ff01cd1058e
Nicolas Cotte <Nicolas.Cotte.1@etu.unige.ch>
2018-04-27 13:29:46 +0200

    Fix DB

--- Jenkins/scripts/database/runDatabase.sh
+++ Jenkins/scripts/database/runDatabase.sh
@@ -3,12 +3,12 @@
 CONTAINER_NAME='concealed_cader'
 IMAGE_NAME='academi-co-mysql'
 
-ssh cotte4@localhost 'if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
  	docker build -t $IMAGE_NAME `dirname "$0"`
 fi
 
 if [ "$(docker ps -q -f name=<name>)" ]; then
 	docker rm -vf $CONTAINER_NAME
-fi'
+fi
 
 docker run -p 13306:3306 --name=$CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=admin -d $IMAGE_NAME
