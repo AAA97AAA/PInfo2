5479456f172e715439afa9a6c360d3a9a8a45822
Nicolas Cotte <Nicolas.Cotte.1@etu.unige.ch>
2018-04-27 11:04:07 +0200

    Add DB

--- Jenkins/scripts/database/restartDatabase.sh
+++ Jenkins/scripts/database/restartDatabase.sh
@@ -3,8 +3,6 @@
 CONTAINER_NAME='concealed_cader'
 IMAGE_NAME='academi-co-mysql'
 
-if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
- 	docker build -t $IMAGE_NAME .
-fi
+docker rm -vf $CONTAINER_NAME
 
 docker run -p 13306:3306 --name=$CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=admin -d $IMAGE_NAME
