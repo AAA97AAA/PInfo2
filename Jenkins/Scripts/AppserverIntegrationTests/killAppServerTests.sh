0c0adec6a456f8f32827a239a89b941bb4134e25
Nicolas Cotte <Nicolas.Cotte.1@etu.unige.ch>
2018-04-30 16:56:43 +0200

    Split Unit and Integration Tests for Maven

--- Jenkins/Scripts/AppserverIntegrationTests/killAppServerTests.sh
+++ Jenkins/Scripts/AppserverIntegrationTests/killAppServerTests.sh
@@ -3,10 +3,6 @@
 CONTAINER_NAME='bodacious_goren'
 IMAGE_NAME='academi-co-test-wildfly'
 
-if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-	docker rm -vf $CONTAINER_NAME
-fi
-
 if [ "$(docker images -q $IMAGE_NAME)" ]; then
  	docker rmi -f $IMAGE_NAME
 fi
