393c3ff312eb739950ac0e1630e6611fb71f5b4a
Nicolas Cotte <Nicolas.Cotte.1@etu.unige.ch>
2018-04-30 09:21:45 +0200

    Fix runAppServer.sh

--- Jenkins/Scripts/Appserver/runAppServer.sh
+++ Jenkins/Scripts/Appserver/runAppServer.sh
@@ -13,5 +13,5 @@ if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
 fi
 
 if [ !"$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-	docker run --ip="172.18.0.2" --net=$NETWORK_NAME -p 18080:8080 -p 19990:9990 -p 18787:8787 -v "/tmp/docker-deploy:/opt/jboss/wildfly/standalone/deployments/:rw" -d $IMAGE_NAME
+	docker run --ip="172.18.0.2" --net=$NETWORK_NAME -p 18080:8080 -p 19990:9990 -p 18787:8787 -v "/tmp/docker-deploy:/opt/jboss/wildfly/standalone/deployments/:rw" --name=$CONTAINER_NAME -d $IMAGE_NAME
 fi
