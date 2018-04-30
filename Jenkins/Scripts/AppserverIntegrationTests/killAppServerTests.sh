505ca2d319147b0c5e6ffcacc2df43d211c4ffc0
Nicolas Cotte <Nicolas.Cotte.1@etu.unige.ch>
2018-04-30 15:23:45 +0200

    Remove unused docker images

--- Jenkins/Scripts/AppserverIntegrationTests/killAppServerTests.sh
+++ Jenkins/Scripts/AppserverIntegrationTests/killAppServerTests.sh
@@ -5,3 +5,7 @@ CONTAINER_NAME='bodacious_goren'
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
 	docker rm -vf $CONTAINER_NAME
 fi
+
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
+ 	docker rmi -f $IMAGE_NAME
+fi
