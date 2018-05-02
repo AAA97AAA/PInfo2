f1a71e6f0959a6b76651fdc2c8cdf6b076f3657a
Aslam CADER <aslam.cader@protonmail.com>
2018-05-02 15:36:04 +0200

    adding JSON content in person.js in the aim of simulating REST calls (done in pair programming with Selim)

--- academi-co/src/main/webapp/script/person.js
+++ academi-co/src/main/webapp/script/person.js
@@ -272,3 +272,151 @@ var app = angular.module('app', [])
   }
 }]);
 
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+
+      $scope.myWelcome = {
+    		  "title": "Does anyone know how to do partial derivative in matlab ?",
+    		  "answers": [
+    		  {
+    		  "id": "25",
+    		  "author": "Angelina Jolie",
+    		  "content": "Hi, try the documentation using help command of matlab, the function is called diff(f,x) as I remember",
+    		  "creationDate": "2018-02-04",
+    		  "upvoters": "SoftAware",
+    		  "downvoters": "SoftAware2",
+    		  "score": "52",
+    		  "isBanned": "false"
+    		  },
+    		  {
+    		  "id": "27",
+    		  "author": "Jennifer Aniston",
+    		  "content": "Which version of matlab are you using ? There's a new function for partial derivations that is faster than diff in the 2017 version of matlab",
+    		  "creationDate": "2018-02-05",
+    		  "upvoters": "SoftAware",
+    		  "downvoters": "SoftAware2",
+    		  "score": "10",
+    		  "isBanned": "false"
+    		  }
+    		  ],
+    		  "subject": "Mathematics",
+    		  "language": "English",
+    		  "topics": [
+    		  {
+    		  "id": "42",
+    		  "name": "Partial Derivative"
+    		  },
+    		  {
+    		  "id": "43",
+    		  "name": "Matlab"
+    		  },
+    		  {
+    		  "id": "44",
+    		  "name": "Integrals"
+    		  }
+    		  ],
+    		  "content": "I'm looking for a function that does partial derivations so I can use them for some integrals in matlab. Thanks for helping",
+    		  "creationDate": "2018-02-01",
+    		  "upvoters": "SoftAware3",
+    		  "downvoters": "softAware4",
+    		  "score": "-5",
+    		  "isBanned": "false"
+    		  }
+});
+
+
+var app = angular.module('academi-co', [])
+
+
+app
+.controller('profilePageLoad', function($scope, $http) {
+
+
+	$scope.myProfileData =       {
+	        "author": "Brad Pitt",
+	        "bio": "I'm an American actor and film producer. I has received multiple awards and nominations including an Academy Award as producer under my own company Plan B Entertainment.",
+	        "posts":
+	            [
+	                {
+	                 "author": "Brad Pitt",
+	                 "title": "Is matlab usefull for partial derivations?",
+	                 "content": "Hi, i'm actually working on a project group for maths stuffs, and i'd like to know if using matlab for partial derivations will be easy to use",
+	                 "creationDate": "2018-02-04",
+	                 "upvoters": "SoftAware",
+	                 "downvoters": "SoftAware2",
+	                 "score": "52",
+	                 "isBanned": "false",
+	                 "subject": "Mathematics",
+	                 "language": "English",
+	                 "topics": 
+	                     [
+	                         {
+	                          "id": "42",
+	                          "name": "Partial Derivative"
+	                         },
+	                         {
+	                          "id": "43",
+	                          "name": "Matlab"
+	                         },
+	                         {
+	                          "id": "44",
+	                          "name": "Integrals"
+	                         }
+	                      ]
+	               },
+	               { 
+	                "author": "Brad Pitt", 
+	                "content": "To answer your question: you need to go to preferences and uncheck the section apply this to all files",
+	                "creationDate": "2018-11-22",
+	                "upvoters": "SoftAware",
+	                "downvoters": "SoftAware2",
+	                "score": "34",
+	                "isBanned": "false",
+	                "language": "English"
+	               },
+	                { 
+	                "author": "Brad Pitt", 
+	                 "title": "Does anyone know how to do partial derivations in matlab?",
+	                 "content": "I'm looking for a function that does partial derivations so I can use them for some integrals in matlab. Thanks for helping",
+	                 "creationDate": "2018-07-12",
+	                 "upvoters": "SoftAware",
+	                 "downvoters": "SoftAware2",
+	                 "score": "2",
+	                 "isBanned": "false",
+	                 "subject": "Mathematics",
+	                 "language": "English",
+	                 "topics": 
+	                      [
+	                     {
+	                      "id": "42",
+	                      "name": "Partial Derivative"
+	                     },
+	                     {
+	                      "id": "43",
+	                      "name": "Matlab"
+	                     },
+	                     {
+	                      "id": "44",
+	                      "name": "Integrals"
+	                     }
+	                   ]
+	               }
+	                 ]
+	        };
+	
+
+
+});
+
+/*
+moduleAngular
+.controller('profilePageLoad', function($scope, $http) {
+	$http.get("academi-co.ch/profilePage")
+	.then(function(response) {
+		$scope.myProfileData = response.data;
+	})
+});
+
+*/
+
+
