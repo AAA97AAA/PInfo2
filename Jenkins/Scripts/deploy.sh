faa9878fef3b5e0ac8fc62f949f4e164180bf0d2
Nicolas Cotte <Nicolas.Cotte.1@etu.unige.ch>
2018-05-02 17:08:48 +0200

    Add Deploy .war in Jenkinsfile

---
+++ Jenkins/Scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOCKER_DEPLOY='/tmp/server/tmp/docker-deploy'
+
+if [[ -e $DOCKER_DEPLOY/academi-co.war ]]; then
+    rm -f $DOCKER_DEPLOY/academi-co.war
+fi
+if [[ -e $DOCKER_DEPLOY/academi-co.war.deployed ]]; then
+    rm -f $DOCKER_DEPLOY/academi-co.war.deployed
+fi
+if [[ -e academi-co/target/academi-co.war ]]; then
+    cp academi-co/target/academi-co.war $DOCKER_DEPLOY
+fi
