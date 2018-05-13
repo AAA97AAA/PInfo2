7204d6067a0f8f66ce3013658e0a88c900da4ba1
kaikoveritch <kaiko.goren@gmail.com>
2018-05-13 16:35:05 +0200

    Attempt 3 to make the tests pass (should fail).

--- academi-co/src/main/java/dom/documentsManager/ConcreteDocument.java
+++ academi-co/src/main/java/dom/documentsManager/ConcreteDocument.java
@@ -108,12 +108,14 @@ public class ConcreteDocument implements Document, Serializable {
 			stream.read(buffer);
 		} catch (IOException e) {
 			logger.error("Could not read stream.", e);
+			e.printStackTrace();
 			return null;
 		}
 		try {
 			stream.close();
 		} catch (IOException e) {
 			logger.error("Could not close stream.", e);
+			e.printStackTrace();
 		}
 		return buffer;
 	}
