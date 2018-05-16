466bc9305c8698dccabbcaf847e8872cc7332156
Selim.Selimi <selim.selimi@etu.unige.ch>
2018-05-16 10:30:31 +0200

    Revisited Settings page and Post thread page

--- academi-co/src/main/webapp/js/controllers/script-searchbar.js
+++ academi-co/src/main/webapp/js/controllers/script-searchbar.js
@@ -83,3 +83,57 @@ function newElement2() {
 	    }
 	  }
 	}
+
+//From there, to avoid conflict with search bar dropdown, we use this for post thread page (PTP)
+//Create a new list item when clicking on the "Add" button
+function newElementPTP1() {
+var li = document.createElement("li");
+var inputValue = document.getElementById("myInputPTP1").value;
+var t = document.createTextNode(inputValue);
+li.appendChild(t);
+if (inputValue === '') {
+alert("You must write something!");
+} else {
+document.getElementById("myULPTP1").appendChild(li);
+}
+document.getElementById("myInputPTP1").value = "";
+
+var span = document.createElement("SPAN");
+var txt = document.createTextNode("\u00D7");
+span.className = "close";
+span.appendChild(txt);
+li.appendChild(span);
+
+for (i = 0; i < close.length; i++) {
+close[i].onclick = function() {
+ var div = this.parentElement;
+ div.style.display = "none";
+}
+}
+}
+
+function newElementPTP2() {
+	  var li = document.createElement("li");
+	  var inputValue = document.getElementById("myInputPTP2").value;
+	  var t = document.createTextNode(inputValue);
+	  li.appendChild(t);
+	  if (inputValue === '') {
+	    alert("You must write something!");
+	  } else {
+	    document.getElementById("myULPTP2").appendChild(li);
+	  }
+	  document.getElementById("myInputPTP2").value = "";
+
+	  var span = document.createElement("SPAN");
+	  var txt = document.createTextNode("\u00D7");
+	  span.className = "close";
+	  span.appendChild(txt);
+	  li.appendChild(span);
+
+	  for (i = 0; i < close.length; i++) {
+	    close[i].onclick = function() {
+	      var div = this.parentElement;
+	      div.style.display = "none";
+	    }
+	  }
+	}
