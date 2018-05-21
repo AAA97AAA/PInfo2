03dee89bc6f22f6e066c4b82617e8ac8bcbed535
Selim.Selimi <selim.selimi@etu.unige.ch>
2018-05-21 20:16:53 +0200

    Change lots of little stuff (w/ nervosity)

--- academi-co/src/main/webapp/js/controllers/script-postThreadPage.js
+++ academi-co/src/main/webapp/js/controllers/script-postThreadPage.js
@@ -19,18 +19,20 @@ close[i].onclick = function() {
 }
 }
 
+//From there, to avoid conflict with search bar dropdown, we use this for post thread page (PTP)
 //Create a new list item when clicking on the "Add" button
-function newElement1() {
+//Create a new list item when clicking on the "Add" button
+function newElementPTP1() {
 var li = document.createElement("li");
-var inputValue = document.getElementById("myInput1").value;
+var inputValue = document.getElementById("myInputPTP1").value;
 var t = document.createTextNode(inputValue);
 li.appendChild(t);
 if (inputValue === '') {
- alert("You must write something!");
+alert("You must write something!");
 } else {
- document.getElementById("myUL1").appendChild(li);
+document.getElementById("myULPTP1").appendChild(li);
 }
-document.getElementById("myInput1").value = "";
+document.getElementById("myInputPTP1").value = "";
 
 var span = document.createElement("SPAN");
 var txt = document.createTextNode("\u00D7");
@@ -39,24 +41,24 @@ span.appendChild(txt);
 li.appendChild(span);
 
 for (i = 0; i < close.length; i++) {
- close[i].onclick = function() {
-   var div = this.parentElement;
-   div.style.display = "none";
- }
+close[i].onclick = function() {
+ var div = this.parentElement;
+ div.style.display = "none";
+}
 }
 }
 
-function newElement2() {
+function newElementPTP2() {
 	  var li = document.createElement("li");
-	  var inputValue = document.getElementById("myInput2").value;
+	  var inputValue = document.getElementById("myInputPTP2").value;
 	  var t = document.createTextNode(inputValue);
 	  li.appendChild(t);
 	  if (inputValue === '') {
 	    alert("You must write something!");
 	  } else {
-	    document.getElementById("myUL2").appendChild(li);
+	    document.getElementById("myULPTP2").appendChild(li);
 	  }
-	  document.getElementById("myInput2").value = "";
+	  document.getElementById("myInputPTP2").value = "";
 
 	  var span = document.createElement("SPAN");
 	  var txt = document.createTextNode("\u00D7");
@@ -71,7 +73,3 @@ function newElement2() {
 	    }
 	  }
 	}
-
-
-
-
