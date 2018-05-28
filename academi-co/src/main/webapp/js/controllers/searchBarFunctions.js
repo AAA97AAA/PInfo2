aa00b7334d2353c9b733eaaf2f2f306c7233c78d
Selim.Selimi <selim.selimi@etu.unige.ch>
2018-05-28 16:43:09 +0200

    Partially finished the control of user input in advance search

--- academi-co/src/main/webapp/js/controllers/searchBarFunctions.js
+++ academi-co/src/main/webapp/js/controllers/searchBarFunctions.js
@@ -4,18 +4,28 @@ var i;
 for (i = 0; i < myNodelist.length; i++) {
 var span = document.createElement("SPAN");
 var txt = document.createTextNode("\u00D7");
-span.className = "close";
+span.className = "closePrimaryTag";
 span.appendChild(txt);
 myNodelist[i].appendChild(span);
 }
 
 //Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
+var closePrimaryTag = document.getElementsByClassName("close");
 var i;
-for (i = 0; i < close.length; i++) {
-close[i].onclick = function() {
+for (i = 0; i < closePrimaryTag.length; i++) {
+ closePrimaryTag[i].onclick = function() {
  var div = this.parentElement;
  div.style.display = "none";
+}
+}
+
+//Click on a close button to hide the current list item
+var closeSecondaryTag = document.getElementsByClassName("close");
+var j;
+for (j = 0; j < closeSecondaryTag.length; j++) {
+closeSecondaryTag[j].onclick = function() {
+var div = this.parentElement;
+div.style.display = "none";
 }
 }
 
@@ -26,11 +36,17 @@ function newElement1() {
 	var inputValue = document.getElementById("primarytag").value; 
 	var t = document.createTextNode(inputValue);
 	li.appendChild(t);
-
+	var containerPrimaryTag = document.getElementById("myUL1")
+	var size1 = containerPrimaryTag.getElementsByTagName("li").length;
+	
 	if (inputValue === 'Select primarytag:') {
 		alert("You must select something!");
-	} else {
-	document.getElementById("myUL1").appendChild(li);
+	} 
+	else if (size1 < 1) {		
+		containerPrimaryTag.appendChild(li);
+	} 
+	else {
+		alert("You can't add more than 1 primary tag");
 	}
 
 	document.getElementById("primarytag").value;
@@ -40,40 +56,48 @@ function newElement1() {
 	span.className = "close";
 	span.appendChild(txt);
 	li.appendChild(span);
-
-	for (i = 0; i < close.length; i++) {
-		close[i].onclick = function() {
-			var div = this.parentElement;
-			div.style.display = "none";
+	
+	for (i = 0; i < closePrimaryTag.length; i++) {
+		closePrimaryTag[i].onclick = function() { 
+			containerPrimaryTag.removeChild(containerPrimaryTag.childNodes[i]);
 		}
 	}
 }
 
+
 /* Function for secondary tag in advanced search: when click in Add button */
 function newElement2() {
-	  var li = document.createElement("li");
-	  var inputValue = document.getElementById("secondarytag").value;
-	  var t = document.createTextNode(inputValue);
-	  li.appendChild(t);
-	  if (inputValue === 'Select secondarytag:') {
-	  	alert("You must select something!");
-	  } else {
-	    document.getElementById("myUL2").appendChild(li);
-	  }
-	  document.getElementById("secondarytag").value;
+	var li = document.createElement("li");
+	var inputValue = document.getElementById("secondarytag").value; 
+	var t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	
+	var size2 = document.getElementById("myUL2").getElementsByTagName("li").length;
+	
+	if (inputValue === 'Select secondarytag:') {
+		alert("You must select something!");
+	} 
+	else if (size2 < 3) {		
+		document.getElementById("myUL2").appendChild(li);
+	} 
+	else {
+		alert("You can't add more than 3 secondary tag");
+	}
 
-	  var span = document.createElement("SPAN");
-	  var txt = document.createTextNode("\u00D7");
-	  span.className = "close";
-	  span.appendChild(txt);
-	  li.appendChild(span);
+	document.getElementById("secondarytag").value;
 
-	  for (i = 0; i < close.length; i++) {
-	    close[i].onclick = function() {
-	      var div = this.parentElement;
-	      div.style.display = "none";
-	    }
-	  }
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	li.appendChild(span);
+	
+	for (j = 0; j < closeSecondaryTag.length; j++) {
+		closeSecondaryTag[j].onclick = function() { 
+			var div = this.parentElement;
+			document.getElementById("myUL2").removeChild(document.getElementById("myUL2").childNodes[j]);
+		}
+	}
 }
 
 /* function that shows or hide the advanced research element */
